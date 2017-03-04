6a7769111cd908bb46f87365b187531117fd4eaa
Amano Shohei <amano@Amano-no-MacBook-Pro.local>
2017-03-04 18:42:48 +0900

    Add Heroku chatbot test

---
+++ run.py
@@ -0,0 +1,14 @@
+"""This is a test program."""
+# coding: utf-8
+
+from slackbot.bot import Bot
+
+def main():
+    """
+    Slackbot test
+    """
+    bot = Bot()
+    bot.run()
+
+if __name__ == '__main__':
+    main()
---
+++ slackbot_settings.py
@@ -0,0 +1,4 @@
+"""This is a test program."""
+API_TOKEN = 'xoxb-149014797505-Z1LUJAnUOYTUuB44ekZxaLx5'
+
+DEFAULT_REPLY = '何言ってんの？'
