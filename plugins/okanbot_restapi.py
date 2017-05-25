83e8a4a4c09b4ad5913d8dc898f140da1579c5e6
Amano Shohei <amano@Amano-no-MacBook-Pro.local>
2017-05-25 23:47:30 +0900

    Functional improvement 8

--- plugins/okanbot_restapi.py
+++ plugins/okanbot_restapi.py
@@ -72,8 +72,7 @@ def search_weather(message):
         geocoder_json = geocoder_api.response_data.json()
         if 'Error' in geocoder_json:
             raise Exception('その場所知らない・・・(´・ω・｀)')
-        print(geocoder_json['Feature'])
-        print(geocoder_json['Feature']['Geometry'])
+        print((geocoder_json['Feature'])['Geometry'])
         coordinates = ((geocoder_json['Feature'])['Geometry'])['Coordinates']
 
         staticmap_api_params = {
