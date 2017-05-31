6efc2ee3dfefcccbc60ea779a4ab49f8156551e6
Amano Shohei <amano@Amano-no-MacBook-Pro.local>
2017-05-31 23:34:31 +0900

    test

--- plugins/okanbot_restapi.py
+++ plugins/okanbot_restapi.py
@@ -96,7 +96,7 @@ def search_weather(message):
         slackapi_params = {
             'token': key_slackbot,
             'channels': 'C5CJE5YBA',
-            'content': image_obj
+            'file': image_obj
         }
         print('手前')
         #resp = requests.post(url_slackapi, data=slackapi_params, files={'file': ('weather.jpg', image_obj, 'image/jpeg')})
