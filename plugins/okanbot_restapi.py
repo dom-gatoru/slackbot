72bc45b6e07ff8b19fedc123d3630f5b0a8902df
Amano Shohei <amano@Amano-no-MacBook-Pro.local>
2017-05-31 23:57:37 +0900

    test

--- plugins/okanbot_restapi.py
+++ plugins/okanbot_restapi.py
@@ -90,13 +90,13 @@ def search_weather(message):
         staticmap_api.api_request(staticmap_api_params)
         image_obj = Image.open(BytesIO(staticmap_api.response_data.content), 'r')
         #resp_obj = BytesIO()
-        #image_obj.save(resp_obj, format='jpg')
+        image_obj.save('weather.jpg', format='jpg')
         #resp_obj.seek(0)
         #image_obj.seek(0)
         slackapi_params = {
             'token': key_slackbot,
             'channels': 'C5CJE5YBA',
-            'content': staticmap_api.response_data.content
+            'content': Image.open('weather.jpg')
         }
         print('手前')
         #resp = requests.post(url_slackapi, data=slackapi_params, files={'file': ('weather.jpg', image_obj, 'image/jpeg')})
